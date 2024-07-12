6effa92cc9748e5340babea4a2bd99d33751bc04
kumanboy <husandavronov13@gmail.com>
2024-07-12 17:31:29 +0500

    get put

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/userModel.js"
 import asyncHandler from "../middlewares/asyncHandler.js";
 // bcryptjs import qilamiz
 import bcrypt from "bcryptjs"
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
     const {username, email, password} = req.body
@@ -23,6 +24,7 @@ const createUser = asyncHandler(async (req, res) => {
 
     try {
         await newUser.save()
+        createToken(res, newUser._id)
         res.status(201).json({
             _id: newUser._id,
             username: newUser.username,
@@ -35,5 +37,72 @@ const createUser = asyncHandler(async (req, res) => {
     }
 })
 
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
 
-export {createUser}
+// mavjud user email tekshiradigan kod
+
+    const existingUser = await User.findOne({email})
+
+    if (existingUser) {
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password)
+        if (isPasswordValid) {
+            createToken(res, existingUser._id)
+            res.status(201).json({
+                _id: existingUser._id,
+                username: existingUser.username,
+                email: existingUser.email,
+                isAdmin: existingUser.isAdmin
+            })
+        }
+    }
+})
+
+const logoutUser = asyncHandler(async (req,res)=>{
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0),
+    })
+    res.status(200).send("User Logged out")
+})
+
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.send(users)
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    if (user) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+
+const updateCurrentUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    if (user) {
+        user.username = req.body.username || user.username
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+        const updatedUser = await user.save()
+        res.json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+export {createUser, loginUser,logoutUser,getAllUsers,getCurrentUser,updateCurrentUser}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,19 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import {
+    createUser,
+    getAllUsers,
+    getCurrentUser,
+    loginUser,
+    logoutUser,
+    updateCurrentUser
+} from "../controllers/userController.js";
+import {authenticate, authorizeAdmin} from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
-router.route('/').post(createUser)
+router.route('/').post(createUser).get(authenticate, authorizeAdmin,getAllUsers)
+router.post('/auth', loginUser)
+router.post('/logout', logoutUser)
+router.route('/profile').get(authenticate, getCurrentUser).put(authenticate,updateCurrentUser)
 
 export default router
