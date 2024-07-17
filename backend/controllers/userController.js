195f98d28edc72af90c72f66e85d52927753c4dd
kumanboy <husandavronov13@gmail.com>
2024-07-17 11:19:57 +0500

    ADMIN side

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -91,7 +91,8 @@ const updateCurrentUser = asyncHandler(async (req, res) => {
         user.username = req.body.username || user.username
         user.email = req.body.email || user.email
         if (req.body.password) {
-            user.password = req.body.password
+            const salt = await bcrypt.genSalt(12)
+            user.password = await bcrypt.hash(req.body.password, salt);
         }
         const updatedUser = await user.save()
         res.json({
@@ -105,4 +106,36 @@ const updateCurrentUser = asyncHandler(async (req, res) => {
         throw new Error("User not found")
     }
 })
-export {createUser, loginUser,logoutUser,getAllUsers,getCurrentUser,updateCurrentUser}
+
+const deleteUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        if (user.isAdmin) {
+            res.status(400);
+            throw new Error("Cannot delete admin user");
+        }
+
+        await User.deleteOne({ _id: user._id });
+        res.json({ message: "User removed" });
+    } else {
+        res.status(404);
+        throw new Error("User not found.");
+    }
+});
+
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select("-password")
+
+    if (user){
+        res.json(user)
+    }else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+
+const updateUserById = asyncHandler(async (req,res)=>{
+//     TODO: Update user
+})
+export {createUser, loginUser,logoutUser,getAllUsers,getCurrentUser,updateCurrentUser,deleteUserById,getUserById}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import {
-    createUser,
+    createUser, deleteUserById,
     getAllUsers,
-    getCurrentUser,
+    getCurrentUser, getUserById,
     loginUser,
     logoutUser,
     updateCurrentUser
@@ -15,5 +15,13 @@ router.route('/').post(createUser).get(authenticate, authorizeAdmin,getAllUsers)
 router.post('/auth', loginUser)
 router.post('/logout', logoutUser)
 router.route('/profile').get(authenticate, getCurrentUser).put(authenticate,updateCurrentUser)
+
+// ADMIN ROUTES
+
+router.route('/:id')
+    .delete(authenticate, authorizeAdmin,deleteUserById)
+    .get(authenticate, authorizeAdmin,getUserById)
+
+
 
 export default router
